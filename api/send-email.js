26050fa99e1502b57e5f8792900e648632decba4
zubairrafi <walleeva2018@gmail.com>
2025-05-31 12:36:16 +0600

    Vercel config fixed
    Signed-off-by: zubairrafi <walleeva2018@gmail.com>

--- api/send-email.js
+++ api/send-email.js
@@ -1,10 +1,16 @@
-// api/send-email.js
-
 const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
